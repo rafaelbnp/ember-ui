5586613fde1fcc65321358c91e9242310939d2f2
Rafael de Paiva <rafaelpaivabh@gmail.com>
2021-10-18 00:31:14 +0200

    Add users index controller unit tests

--- tests/unit/controllers/users/index-test.js
+++ tests/unit/controllers/users/index-test.js
@@ -1,12 +1,33 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Controller | users/index', function(hooks) {
+module('Unit | Controller | users/index', function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
-  test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:users/index');
-    assert.ok(controller);
+  test('it toggles showArchivedUsers', function (assert) {
+    const controller = this.owner.lookup('controller:users/index');
+
+    assert.notOk(
+      controller.showArchivedUsers,
+      'showArchivedUsers is initially false'
+    );
+
+    controller.toggleShowArchivedUsers();
+
+    assert.ok(
+      controller.showArchivedUsers,
+      'showArchivedUsers is toggled to true'
+    );
+  });
+
+  test('it can filter archived users', function (assert) {
+    const controller = this.owner.lookup('controller:users/index');
+
+    controller.model = [{ archived: false }, { archived: true }];
+
+    assert.equal(controller.users.length, 1);
+
+    controller.showArchivedUsers = true;
+    assert.equal(controller.users.length, 2);
   });
 });
